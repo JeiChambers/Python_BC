6a0cd4d316de1f82d34a652b305b3d923041261c
JeiChambers <jeilee88@outlook.com>
2019-11-14 19:03:10 -0600

    Updated .gitignore and pushed finished version of dadjokes.py

--- dadjokes.py
+++ dadjokes.py
@@ -1,11 +1,34 @@
 import requests
+from pyfiglet import figlet_format
+from termcolor import colored
+from random import choice
+
+# Functions:
+def title(message):
+    header = figlet_format(message)
+    header = colored(header, color="blue")
+    print(header)
+
+title("Dad Jokes Pro!")
+
+topic = input("Let me tell you a joke! Give me a topic: ")
 url = "https://icanhazdadjoke.com/search"
 
-response = requests.get(
-url,
-headers={"Accept": "application/json"},
-params={"term": "cat", "limit": 1}
-)
+res = requests.get(
+    url,
+    headers={"Accept": "application/json"},
+    params={"term":topic}
+).json()
 
-data = response.json()
-print(data["results"])
+num_jokes = res["total_jokes"]
+results = res["results"]
+if num_jokes > 1:
+    print(f"I found {num_jokes} jokes about {topic}! Here's one of them:")
+    print(choice(results)['joke'])
+elif num_jokes == 1:
+    print(f"I found {num_jokes} joke about {topic}! Here it is: ")
+    print(results[0]['joke'])
+else:
+    print(f"There are no jokes about {topic}!")
+
+
